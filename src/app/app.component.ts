c71e2a6a90f8b526809633ff2e4c979cbd74a873
Jason Royle <me@jasonroyle.com>
2024-02-21 22:06:44 +0000

    Configured project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   standalone: true,
   imports: [RouterModule],
-  selector: 'curriculum-vitae-root',
+  selector: 'cv-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
